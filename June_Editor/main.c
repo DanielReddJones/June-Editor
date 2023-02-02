c9b25e63a7a3e3fceba8633253636f4950333e4b
Daniel Jones <104509116+DanielReddJones@users.noreply.github.com>
2023-02-02 11:22:23 -0600

    Initial Commit
    Added license agreement to startup

---
+++ June_Editor/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("June Editor  Copyright (C) 2023  Daniel Jones
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
+    This is free software, and you are welcome to redistribute it
+    under certain conditions; type `show c' for details.\n");
+    
+    
+    
+    return 0;
+}
