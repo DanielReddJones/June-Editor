23cba2d6fe906afd53f45b31c735fed46c1def02
Daniel Jones <104509116+DanielReddJones@users.noreply.github.com>
2023-02-02 11:43:58 -0600

    Added Makefile

--- June_Editor/main.c
+++ June_Editor/main.c
@@ -1,12 +1,21 @@
+/*
+Author: Daniel Jones
+Last edited: 2/2/2023 11:37 AM
+Editor: Vim
+Compiler: GCC
+Operating System: WSL2 Windows 10
+Purpose: Text editor written in C. Part of a course "How to make your own text editor"(https://viewsourcecode.org/snaptoken/kilo/01.setup.html)
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main()
 {
-    printf("June Editor  Copyright (C) 2023  Daniel Jones
-    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
-    This is free software, and you are welcome to redistribute it
-    under certain conditions; type `show c' for details.\n");
+    printf("June Editor  Copyright (C) 2023  Daniel Jones\
+    \nThis program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\
+    \nThis is free software, and you are welcome to redistribute it\
+    \nunder certain conditions; type `show c' for details.\n");
     
     
     
